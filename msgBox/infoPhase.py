842b977d16bf036689f7c45b4f6c90e3d6ca0303
Alessio Graziano <a.graziano@lparchaeology.com>
2021-07-21 13:33:37 +0000

    Update infoPhase.py

--- msgBox/infoPhase.py
+++ msgBox/infoPhase.py
@@ -7,14 +7,14 @@ class InfoPhase(QMessageBox):
 
         self.setWindowTitle("")
         self.setText("VALID VALUE Phase \n\n"
-                        "◊ Phase 1 \n\n"
-                        "◊ Phase 2 \n\n"
-                        "◊ Phase 3 \n\n"
-                        "◊ Phase 4 \n\n"
-                        "◊ Phase 5 \n\n"
-                        "◊ Phase 6 \n\n"
-                        "◊ Phase 7 \n\n"
-                        "◊ Phase 8 \n\n"
+                        "◊  1 \n\n"
+                        "◊  2 \n\n"
+                        "◊  3 \n\n"
+                        "◊  4 \n\n"
+                        "◊  5 \n\n"
+                        "◊  6 \n\n"
+                        "◊  7 \n\n"
+                        "◊  8 \n\n"
                         "◊ Unknown")
 
         self.setIcon(QMessageBox.Information)
